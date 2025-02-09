bf9329e4208d4cfb4e53c377d9be47a3d3454b92
benj9000 <49900294+benj9000@users.noreply.github.com>
2025-02-09 01:42:59 +0100

    Initialize repository with a Nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "HTML elements showcase";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonVersions = {
+          default = pkgs.python314;
+          python312 = pkgs.python312;
+          python313 = pkgs.python313;
+          python314 = pkgs.python314;
+        };
+        mkPythonShell = devShellName: pythonPackage:
+          let venvDirectory = "venv"; in
+          pkgs.mkShell {
+            name = "python-uv-development-environment";
+            nativeBuildInputs = [ pythonPackage pkgs.uv ];
+            UV_PROJECT_ENVIRONMENT = venvDirectory;
+            UV_PYTHON_DOWNLOADS = "never";
+            shellHook = ''
+              echo "Start Python development environment with uv"
+              uv sync --python ${pythonPackage}/bin/python
+              if [ -d ${venvDirectory} ]; then
+                  source ${venvDirectory}/bin/activate
+              fi
+            '';
+          };
+      in
+      {
+        devShells = builtins.mapAttrs mkPythonShell pythonVersions;
+      }
+    );
+}
