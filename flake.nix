f7fab8e84fa572bae40efcd6d790081dd9cf96df
benj9000 <49900294+benj9000@users.noreply.github.com>
2025-02-23 02:02:38 +0100

    Add live server scripts to Nix development shell

--- flake.nix
+++ flake.nix
@@ -16,8 +16,13 @@
           python313 = pkgs.python313;
           python314 = pkgs.python314;
         };
+
         mkPythonShell = devShellName: pythonPackage:
-          let venvDirectory = "venv"; in
+          let
+            projectName = "html-element-showcase";
+            venvDirectory = "./venv";
+            outputDirectory = "./dist";
+          in
           pkgs.mkShell {
             name = "python-uv-development-environment";
             nativeBuildInputs =
@@ -45,6 +50,31 @@
                       --add-flags "--print-width 100"
                   '';
                 };
+
+                liveServerScripts = pkgs.symlinkJoin {
+                  name = "live-server-scripts-${projectName}";
+                  paths =
+                    let
+                      liveServerTmuxSessionName = "live server (${projectName})";
+                      startLiveServerScript = pkgs.writeShellScriptBin "start-live-server" ''
+                        if [ ! -d ${outputDirectory} ]; then
+                            echo "Could not detect the directory to serve."
+                            echo "Make sure to call this command from within the project's root directory."
+                            exit 1
+                        fi
+                        ${pkgs.tmux}/bin/tmux new-session -s "${liveServerTmuxSessionName}" -d \
+                            "${pkgs.nodePackages.live-server}/bin/live-server ${outputDirectory}"
+                      '';
+                      stopLiveServerScript = pkgs.writeShellScriptBin "stop-live-server" ''
+                        ${pkgs.tmux}/bin/tmux kill-session -t "${liveServerTmuxSessionName}"
+                      '';
+                      showLiveServerScript = pkgs.writeShellScriptBin "show-live-server" ''
+                        ${pkgs.tmux}/bin/tmux attach-session -t "${liveServerTmuxSessionName}"
+                      '';
+                    in
+                    [ startLiveServerScript stopLiveServerScript showLiveServerScript ];
+                };
+
               in
               [
                 pythonPackage
@@ -52,10 +82,12 @@
                 pkgs.basedpyright
                 pkgs.ruff
                 prettierWithPlugins
+                liveServerScripts
               ];
 
             UV_PROJECT_ENVIRONMENT = venvDirectory;
             UV_PYTHON_DOWNLOADS = "never";
+
             shellHook = ''
               echo "Start Python development environment with uv"
               uv sync --python ${pythonPackage}/bin/python
