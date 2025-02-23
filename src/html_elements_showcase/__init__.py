cbe9503f5d5272a024801d7529e6b6bf503fa2c7
benj9000 <49900294+benj9000@users.noreply.github.com>
2025-02-23 02:53:44 +0100

    Assemble a build script

--- src/html_elements_showcase/__init__.py
+++ src/html_elements_showcase/__init__.py
@@ -1,2 +1,28 @@
+from html_elements_showcase.configuration import DEBUG
+from html_elements_showcase.data_transfer import parse_result_to_template_data
+from html_elements_showcase.data_transfer_objects import (
+    SectionParseResult,
+    SectionTemplateData,
+)
+from html_elements_showcase.fetch import (
+    fetch_page_source,
+    fetch_page_source_debug,
+)
+from html_elements_showcase.parse import parse
+from html_elements_showcase.render import render, write
+
+
+def build(debug: bool) -> None:
+    page_source: str
+    if debug:
+        page_source = fetch_page_source_debug()
+    else:
+        page_source = fetch_page_source()
+    sections: list[SectionParseResult] = parse(page_source)
+    template_data: list[SectionTemplateData] = parse_result_to_template_data(sections)
+    content: str = render(template_data)
+    write(content)
+
+
 def main() -> None:
-    print("Hello from html-elements-showcase!")
+    build(debug=DEBUG)
--- src/html_elements_showcase/configuration.py
+++ src/html_elements_showcase/configuration.py
@@ -14,3 +14,5 @@ MDN_HTML_ELEMENTS_REFERENCE_URL: str = (
 MDN_HTML_ELEMENTS_REFERENCE_CACHE_PATH: Path = (
     DATA_DIRECTORY / "mdn-html-elements-reference.html"
 )
+
+DEBUG: bool = True
